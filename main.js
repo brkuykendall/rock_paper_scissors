c8abba15072b8a632cb8d9c4779572783a4cc248
brkuykendall <kuykendallbrittany594@gmail.com>
2024-02-08 22:25:04 -0600

    Add index.html

---
+++ main.js
@@ -0,0 +1,21 @@
+const choices = ["rock","paper","scissors"]
+
+
+function game () {
+    // play the game
+    // game has five rounds
+    // plays within the console
+}
+
+function playRound () {
+const playerSelection =
+}
+
+function playerChoice () {
+    // get input from player
+}
+
+function computerChoice () {
+        // get input from computer
+    return choices [Math.floor(Math.random()*3)]
+}
